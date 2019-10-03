dddd9e41f5ff80b8081f1b1cdc036e68d5afe245
laurenchilders <chilla17@wfu.edu>
2019-10-03 15:09:12 -0400

    still in class - not complete

---
+++ Faculty.h
@@ -0,0 +1,8 @@
+#ifndef CLASSEXCERCISE_FACULTY_H
+#define CLASSEXCERCISE_FACULTY_H
+class Faculty {
+public:
+    Faculty(); // default constructor
+    Faculty(n,  d, add, dob, deg, salary, ssn);
+};
+#endif //CLASSEXCERCISE_FACULTY_H
--- main.cpp
+++ main.cpp
@@ -3,21 +3,47 @@ using namespace std;
 
 class Faculty{
 public:
+    Faculty(); // default constructor
+    Faculty(string n, string d, string add, string dob, string deg, double salary, double ssn);
     string name;
     string id;
     string address;
     string DoB;
     string degree;
+
 //getter and setter for salary
     double getSalary() {return salary;}
     void setSalary(double salary);
 /* 1) create a getter and setter for each variable in the class*/
+    string getName() {return name};
+    void setName(string name);
 
+    string getId() {return id;}
+    void setId(string id);
+
+    string getAddress() {return address;}
+    void setAddress(string address);
+
+    string getDoB() {return DoB};
+    void setDoB(string DoB);
+
+    string getDegree() {return degree;}
+    void setDegree(string degree);
 
 private:
     double salary;
     double SSN;
 };
+
+Faculty::Faculty(string n, string d, string add, string dob, string deg, double salary, double ssn) {
+    name = n;
+    id = d;
+    address = add;
+    DoB = dob;
+    degree = deg;
+    salary = salary;
+    SSN = ssn;
+}
 
 void Faculty::setSalary(double s){
     salary=s;
